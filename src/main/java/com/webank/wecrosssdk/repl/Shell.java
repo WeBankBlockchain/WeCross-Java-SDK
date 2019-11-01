d93c959f539fea211a40f5089946a85dbb96d823
ancelmo <ancelmo@ANCELMOOA2.webank.com>
2019-11-01 13:24:21 +0800

    remove printStackTrace

--- src/main/java/com/webank/wecrosssdk/repl/Shell.java
+++ src/main/java/com/webank/wecrosssdk/repl/Shell.java
@@ -54,7 +54,7 @@ public class Shell {
             }
         } catch (IOException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+            // e.printStackTrace();
         }
     }
 
