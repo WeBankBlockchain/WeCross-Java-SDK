42aeb228b8613e95d875e8447f1544cfcf1f3ad4
Shareong <740310627@qq.com>
2019-12-26 11:46:38 +0800

    update version (#24)

--- src/main/java/com/webank/wecrosssdk/rpc/common/Version.java
+++ src/main/java/com/webank/wecrosssdk/rpc/common/Version.java
@@ -2,5 +2,5 @@ package com.webank.wecrosssdk.rpc.common;
 
 public class Version {
 
-    public static final String CurrentVersion = "1.0-RC1";
+    public static final String CurrentVersion = "1";
 }
