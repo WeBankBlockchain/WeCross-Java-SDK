de1138fb11efee5a30f967b64be22adbaa7d3069
shareong <740310627@qq.com>
2020-04-14 12:45:41 +0800

    add htlc performance suit

---
+++ src/test/java/com/webank/wecrosssdk/performance/htlc/HTLCPerformanceTest.java
@@ -0,0 +1,99 @@
+package com.webank.wecrosssdk.performance.htlc;
+
+import com.webank.wecrosssdk.exception.WeCrossSDKException;
+import com.webank.wecrosssdk.performance.PerformanceManager;
+import com.webank.wecrosssdk.rpc.WeCrossRPC;
+import com.webank.wecrosssdk.rpc.WeCrossRPCFactory;
+import com.webank.wecrosssdk.rpc.service.WeCrossRPCService;
+import java.math.BigInteger;
+
+public class HTLCPerformanceTest {
+
+    public static void usage() {
+        System.out.println("Usage:");
+        System.out.println(
+                " \t java -cp conf/:lib/*:apps/* com.webank.wecrosssdk.performance.htlc.HTLCPerformanceTest [selfPath] [senderAccount0] [sender0] [receiver0] [counterpartyPath] [senderAccount1] [sender1] [receiver1] [count] [qps]");
+        System.out.println("Example:");
+        System.out.println(
+                " \t java -cp conf/:lib/*:apps/* com.webank.wecrosssdk.performance.htlc.HTLCPerformanceTest payment.bcos.htlc bcos 0x55f934bcbe1e9aef8337f5551142a442fdde781c 0x2b5ad5c4795c026514f8317c7a215e218dccd6cf payment.fabric.htlc fabric Admin@org1.example.com User1@org1.example.com 100 10");
+        System.out.println(
+                "======================================================================================================================================");
+        System.out.println(
+                "Notification: 1. the router connected by sdk should config both senders account\n"
+                        + "              2. all htlc contracts should have different asset contracts\n"
+                        + "              3. bcos with bcos, bcos with fabric, fabric with fabric use the same command\n");
+        exit();
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 10) {
+            usage();
+        }
+
+        String selfPath = args[0];
+        String senderAccount0 = args[1];
+        String sender0 = args[2];
+        String receiver0 = args[3];
+        String counterpartyPath = args[4];
+        String senderAccount1 = args[5];
+        String sender1 = args[6];
+        String receiver1 = args[7];
+        BigInteger count = new BigInteger(args[8]);
+        BigInteger qps = new BigInteger(args[9]);
+
+        System.out.println(
+                "FabricPerformanceTest: selfPath is "
+                        + selfPath
+                        + ", senderAccount0 is "
+                        + senderAccount0
+                        + ", sender0 is "
+                        + sender0
+                        + ", receiver0 is "
+                        + receiver0
+                        + ", counterpartyPath is "
+                        + counterpartyPath
+                        + ", senderAccount1 is "
+                        + senderAccount1
+                        + ", sender1 is "
+                        + sender1
+                        + ", receiver1 is "
+                        + receiver1
+                        + ", count is "
+                        + count
+                        + ", qps is "
+                        + qps);
+        try {
+            WeCrossRPC weCrossRPC = loadWeCrossRPC();
+            HTLCTransferSuite suite =
+                    new HTLCTransferSuite(
+                            weCrossRPC,
+                            selfPath,
+                            senderAccount0,
+                            sender0,
+                            receiver0,
+                            counterpartyPath,
+                            senderAccount1,
+                            sender1,
+                            receiver1);
+            PerformanceManager performanceManager = new PerformanceManager(suite, count, qps);
+            performanceManager.run();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        exit();
+    }
+
+    private static WeCrossRPC loadWeCrossRPC() throws Exception {
+        WeCrossRPCService weCrossRPCService = new WeCrossRPCService();
+        try {
+            return WeCrossRPCFactory.build(weCrossRPCService);
+        } catch (WeCrossSDKException e) {
+            throw new Exception("Error: Init wecross service failed: {}" + e.getMessage());
+        }
+    }
+
+    private static void exit() {
+        System.exit(0);
+    }
+}
