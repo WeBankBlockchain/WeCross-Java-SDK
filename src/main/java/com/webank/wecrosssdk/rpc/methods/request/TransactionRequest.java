d86fc907cb3244a242870fec30c1e2d0b2278306
jimmyshi <417711026@qq.com>
2020-07-01 21:28:19 +0800

    update transaction request format (#79)

--- src/main/java/com/webank/wecrosssdk/rpc/methods/request/TransactionRequest.java
+++ src/main/java/com/webank/wecrosssdk/rpc/methods/request/TransactionRequest.java
@@ -5,28 +5,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TransactionRequest {
-    private String path;
     private String method;
     private String[] args;
 
     // Optional args
     // transactionID, peers, etc...
-    private Map<String, Object> options = new HashMap<>();
+    private Map<String, Object> options = new HashMap<String, Object>();
 
     public TransactionRequest() {}
 
-    public TransactionRequest(String path, String method, String[] args) {
-        this.path = path;
+    public TransactionRequest(String method, String[] args) {
         this.method = method;
         this.args = args;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
     }
 
     public String getMethod() {
@@ -60,10 +50,7 @@ public class TransactionRequest {
     @Override
     public String toString() {
         return "TransactionRequest{"
-                + "path='"
-                + path
-                + '\''
-                + ", method='"
+                + "method='"
                 + method
                 + '\''
                 + ", args="
