64bcb66cc5ace78c1a365588db45040604d7c886
JimmyShi22 <417711026@qq.com>
2020-11-09 20:34:07 +0800

    fix mac problems && add mor ci check

--- .ci/ci_check.sh
+++ .ci/ci_check.sh
@@ -7,8 +7,8 @@ set -e
 
 curl -LO https://github.com/WeBankFinTech/WeCross-Java-SDK/releases/download/resources/resources.tar.gz
 tar -zxvf resources.tar.gz
-cp resources src/test/ -r
-rm resources -rf
+cp -r resources src/test/
+rm -rf resources
 
 ./gradlew verifyGoogleJavaFormat
 ./gradlew build
